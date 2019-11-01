465eebe4a38ad96974d0e9a7b1b2f78e5d970358
Saikrishna-git <57249218+Saikrishna-git@users.noreply.github.com>
2019-11-01 17:18:03 +0530

    Update Jummy1.java

--- src/Tummy/Jummy1.java
+++ src/Tummy/Jummy1.java
@@ -5,6 +5,7 @@ public class Jummy1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi how are you");
+		System.out.println("were are you");
 
 	}
 
